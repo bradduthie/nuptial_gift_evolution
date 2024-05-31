9abce5cf048965897c5bbf093ac9634366b489b7
Brad Duthie <brad.duthie@gmail.com>
2024-05-31 14:57:57 +0100

    Iniitialise code for running individual-based model

---
+++ nuptial_rep.c
@@ -0,0 +1,84 @@
+/******************************************************************************/
+/* Program: nuptial_rep.c
+   By: Brad Duthie                                             
+   Description: 
+   Compile: To compile with makefile, type `make' in directory, then hit ENTER
+   Run:     To run, type `./nuptial_rep' on command line, then hit ENTER      */
+/******************************************************************************/
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "nuptials.h"
+
+int main(void){
+
+    int i, time_steps, N, rep, xdim, ydim, K, stats, pid;
+    double Tm, Tf, rejg, mim, mom, gam, mov, a1, lambd, Tm_mu, rg_mu, N_mu;
+    char outfile[20];
+    FILE *fptr; 
+
+    /* ======= VARIABLES BETWEEN THE Xs BELOW ADJUST MODEL PARAMETERS ========*/
+    /*  XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX   */
+    /* =======================================================================*/
+    /* Model parameter values                                                 */
+    /* =======================================================================*/
+    time_steps = 400000;/* Simulation time steps                              */
+    N          = 1000;  /* Population size                                    */
+    Tm         = 1.5;   /* Initial male search time                           */
+    Tf         = 2.0;   /* Initial female processing time                     */
+    rejg       = 0.0;   /* Initial female rejection probability               */
+    mim        = 0.01;  /* Mortality rate for time-in stage                   */
+    mom        = 0.01;  /* Mortality rate for time-out stage                  */
+    gam        = 1.5;   /* Offspring increase for nuptial gift               */
+    mov        = 1.0;   /* Movement parameter                                 */
+    a1         = 1.50;  /* Search time needed to find a gift                  */
+    lambd      = 1.0;   /* Baseline female reproduction                       */
+    xdim       = 1;     /* dimension of x-loc landscape                       */
+    ydim       = 1;     /* dimension of y-loc landscape                       */
+    K          = 1000;  /* Population carrying capacity                       */
+    Tm_mu      = 0.00;  /* Error of offspring Tm from mean parent             */
+    rg_mu      = 0.01;  /* Error of offspring rejg from mean parent           */
+    N_mu       = 0.01;  /* Error of the neutral allele fro mean parent        */
+    /* =======================================================================*/
+
+    /* =======================================================================*/
+    /* Simulation details                                                     */
+    /* =======================================================================*/
+    rep    = 20; /* Simulations run                                         */
+    stats  = 0;    /* 0: end of time step, 1: each time step, 2: all inds     */
+    /* =======================================================================*/
+    /*  XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX   */
+    /* =======================================================================*/
+    srand(time(NULL) ^ (getpid()<<16)); /* Use time to generate random seed */
+
+    i    = 0; /* Loops through different replicate simulations */
+
+    if(stats == 0){
+      pid = getpid();
+      sprintf(outfile, "results_%d.csv", pid);
+      fptr = fopen(outfile, "a+");
+      fprintf(fptr, "mim,mom,gam,mov,a1,lambd,xdim,ydim,K,Tm_init,rg_init,\
+                     Tm_mu,rg_mu,Time,Sex_ratio,Time-in,Tm,Tf,Gift,RejPr,Offs,\
+                     N,Vr_Tm,Vr_Rj,M_m,N_m,Beta,M_males,M_females,\
+                     M_male_gft,F_Mnupt_enc,F_Mnonupt_enc\n");
+      fclose(fptr);
+    }
+
+    while(i < rep){
+
+        nuptials(time_steps, N, Tm, Tf, rejg, mim, mom, gam, mov, a1, lambd, 
+                 xdim, ydim, K, stats, Tm_mu, rg_mu, N_mu);
+                 
+        i++;
+
+        if(i % 4 == 0){
+          gam += 0.1;
+        }
+
+        printf("Rep: %d of %d\n", i, rep);
+    }
+    return 0;
+}
+
---
+++ nuptials.h
@@ -0,0 +1,50 @@
+#include "rand_sampling.h"
+
+void swap_arrays(void **ARRAY_A, void **ARRAY_B);
+
+void male_search(double **inds, int male);
+
+void female_process(double **inds, int female);
+
+void initialise(int N, double Tm, double Tf, double rejg, double mim, 
+                double mom, double gam, double mov, double a1, double lambd, 
+                int xdim, int ydim, double **inds);
+
+void move_inds(double **inds, int xdim, int ydim, int N);
+
+void female_male_int(double **inds, int male, int female);
+
+void ind_mortality(double **inds, int i);
+
+void mortality(double **inds, int N);
+
+void enter_mating_pool(double **inds, int N);
+
+void get_offspring(double **inds, int N, double M_exp);
+
+int count_offspring(double **inds, int N);
+
+int find_dad(double **inds, int N, double dad_ID);
+
+double off_trait(double **inds, int mum_row, int dad_row, int trait_col);
+
+void add_offspring(double **inds, int N, double **offs, int off_N, int traits,
+                   int *ID, double Tm_mu, double rg_mu, double N_mu,
+                   int xdim, int ydim);
+
+void apply_K(double **inds, int N, double **offs, int off_N, int K);
+
+int count_living(double **inds, int N, double **offs, int off_N);
+
+void build_new_N_offs(double **inds, int N, double **offs, int off_N, int new_N,
+                      double **news, int ind_traits);
+
+void build_newN(double **inds, int N, int new_N, double **news, int ind_traits);
+
+void sumstats(double **inds, int N, int ind_traits, int stats, int ts, 
+              int off_N, double N_mu);
+
+void nuptials(int time_steps, int N, double Tm, double Tf, double rejg,
+              double mim, double mom, double gam, double mov, double a1,
+              double lambd, int xdim, int ydim, int K, int stats, 
+              double Tm_mu, double rg_mu, double N_mu);
